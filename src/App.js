f8115e2d805ad3da41ada68074f85eae53d00ee0
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-03-02 17:54:08 -0500

    Added conditionals to handleSubmit

--- src/App.js
+++ src/App.js
@@ -2,15 +2,7 @@ import React, { useState } from "react"
 
   /**
      * Challenge: Connect the form to local state
-     * 
-     * 1. Create a state object to store the 4 values we need to save.
-     * 2. Create a single handleChange function that can
-     *    manage the state of all the inputs and set it up
-     *    correctly
-     * 3. When the user clicks "Sign up", check if the 
-     *    password & confirmation match each other. If
-     *    so, log "Successfully signed up" to the console.
-     *    If not, log "passwords to not match" to the console.
+    
      * 4. Also when submitting the form, if the person checked
      *    the "newsletter" checkbox, log "Thanks for signing
      *    up for our newsletter!" to the console.
@@ -34,10 +26,21 @@ export default function App() {
             }
         })
     }
-    console.log(formData)
 
     function handleSubmit(event) {
         event.preventDefault()
+        if(formData.password === formData.confirmPassword) {
+            alert("Successfully signed up")
+            console.log("Successfully signed up")
+        } else {
+            alert("passwords do not match")
+            console.log("passwords do not match")
+        }
+
+        if(formData.newsletter) {
+            alert("Thanks for signing up for our newsletter")
+            console.log("Thanks for signing up for our newsletter")
+        } 
     }
     
     return (
